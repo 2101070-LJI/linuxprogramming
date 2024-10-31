4a0bf41464777b7a64ef889a6d075215041aa581
2101070-LJI <jungin5258@gmail.com>
2024-10-31 10:21:09 +0900

    Add files via upload

---
+++ C++ 7장 프렌드와 연산자 중복자/프렌드와 연산자 중복_3-2/complex.hpp
@@ -0,0 +1,15 @@
+#ifndef _COMPLEX_HPP_
+#define _COMPLEX_HPP_
+
+class Complex;			//Complex 선언
+
+class Complex{											//Complex클래스
+	int real;
+	int img;
+public:
+	Complex();
+	Complex(int real, int img);
+	void show();
+	Complex operator*(Complex b);	//프렌드 함수 선언
+};
+#endif                                                                
