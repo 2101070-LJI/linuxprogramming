75b1b2ee5072fd980066c802ad3add8bfdc6f3fe
2101070-LJI <jungin5258@gmail.com>
2024-11-11 23:26:21 +0900

    Update circle.cpp

--- ch.8/p.418_1/circle.cpp
+++ ch.8/p.418_1/circle.cpp
@@ -1,12 +1,22 @@
 #include "circle.hpp"
 
-Circle::Circle(int radius){
-    this->radius=radius;
+// Circle 클래스 생성자 구현
+Circle::Circle(int radius) {
+    this->radius = radius; // 전달된 radius 값을 멤버 변수에 설정
 }
-void Circle::Show(){cout<<"반지름이 "<<radius<<"인 ";}
 
-
-NamedCircle::NamedCircle(int radius, string name):Circle(radius){
-    this->name=name;
+// Circle 클래스의 Show 함수 구현
+void Circle::Show() {
+    cout << "반지름이 " << radius << "인 "; // 원의 반지름을 출력
 }
-void NamedCircle::show(){Show(); cout<<name<<endl;}
+
+// NamedCircle 클래스 생성자 구현 (Circle 클래스 상속)
+NamedCircle::NamedCircle(int radius, string name) : Circle(radius) {
+    this->name = name; // 전달된 이름을 멤버 변수에 설정
+}
+
+// NamedCircle 클래스의 show 함수 구현
+void NamedCircle::show() {
+    Show(); // 부모 클래스인 Circle의 Show 함수 호출
+    cout << name << endl; // 원의 이름을 출력
+}
