91ecf740e5ab2636b894a442253432e69c587c7f
2101070-LJI <jungin5258@gmail.com>
2024-11-11 23:27:50 +0900

    Update circle.hpp

--- ch.8/p.418_2/circle.hpp
+++ ch.8/p.418_2/circle.hpp
@@ -1,25 +1,27 @@
-#ifndef _SHAPE_HPP_
+#ifndef _SHAPE_HPP_  // 중복 포함 방지
 #define _SHAPE_HPP_
 #include <iostream>
 #include <string>
 using namespace std;
 
-class Circle{
-    int radius;
+// Circle 클래스 정의
+class Circle {
+    int radius; // 원의 반지름
 public:
-    Circle(int radius);
-    void setRadius(int radius);
-    int getRadius();
-    void Show();
+    Circle(int radius); // 생성자: 반지름을 설정
+    void setRadius(int radius); // 반지름을 설정하는 함수
+    int getRadius(); // 반지름을 반환하는 함수
+    void Show(); // 반지름을 출력하는 함수
 };
 
-class NamedCircle:public Circle{
-    string name;
+// NamedCircle 클래스 정의 (Circle 클래스를 상속)
+class NamedCircle : public Circle {
+    string name; // 원의 이름
 public:
-    NamedCircle();
-    void setName(string name);
-    string getName();
-    void show();
+    NamedCircle(); // 생성자: 기본값을 설정
+    void setName(string name); // 이름을 설정하는 함수
+    string getName(); // 이름을 반환하는 함수
+    void show(); // 원의 반지름과 이름을 출력하는 함수
 };
 
 #endif
