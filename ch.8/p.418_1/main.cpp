6e86c101188c99aeb4988842e9f7927b2ccac3df
2101070-LJI <jungin5258@gmail.com>
2024-11-11 23:26:38 +0900

    Update main.cpp

--- ch.8/p.418_1/main.cpp
+++ ch.8/p.418_1/main.cpp
@@ -1,6 +1,7 @@
 #include "circle.hpp"
-int main(){
-    NamedCircle waffle(3,"waffle");
-    waffle.show();
-    return 0;
+
+int main() {
+    NamedCircle waffle(3, "waffle"); // 반지름 3, 이름 "waffle"인 NamedCircle 객체 생성
+    waffle.show(); // 원의 반지름과 이름을 출력하는 show 함수 호출
+    return 0; // 프로그램 종료
 }
