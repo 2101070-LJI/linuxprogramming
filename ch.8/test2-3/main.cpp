7d67126e3eb8a0ae4a2c4676b5a6d400a9c6c875
2101070-LJI <jungin5258@gmail.com>
2024-11-11 23:30:19 +0900

    Update main.cpp

--- ch.8/test2-3/main.cpp
+++ ch.8/test2-3/main.cpp
@@ -1,10 +1,13 @@
 #include "shape.hpp"
 
 int main() {
-Circle x(0,0,2);
-Rect y(1,1,5,10);
-Triangle z(2,2,5,10);
-x.show();
-y.show();
-z.show();
+    Circle x(0, 0, 2); // Circle 객체 x 생성
+    Rect y(1, 1, 5, 10); // Rect 객체 y 생성
+    Triangle z(2, 2, 5, 10); // Triangle 객체 z 생성
+
+    x.show(); // Circle 객체의 show() 호출
+    y.show(); // Rect 객체의 show() 호출
+    z.show(); // Triangle 객체의 show() 호출
+
+    return 0; // 프로그램 종료
 }
