c8e474ea7a0dcc5eccf28f4610337f368eae32eb
2101070-LJI <jungin5258@gmail.com>
2024-11-14 14:46:45 +0900

    Update main.cpp

--- ch.10/test1_1/main.cpp
+++ ch.10/test1_1/main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-#include <string>
-using namespace std;
-template <class T>
-T getmax(T x[], int n) {
+#include <iostream>        //iostream 라이브러리 포함
+#include <string>          //string 라이브러리 포함
+using namespace std;       //std 생략 
+
+template <class T>          //템플릿 변수 선언 
+T getmax(T x[], int n) {    //가장 큰 매개변수를 찾는 함수 정의
     T max = x[0];
     for (int i = 1; i < n; i++) {
         if (max < x[i]) max = x[i];
@@ -11,10 +12,11 @@ T getmax(T x[], int n) {
 }
 
 int main() {
-    int a[5] = { -5, 10, 30, 20, 6 };
-    double b[4] = { 3.14, 1.5, -6.0, 0.5 };
-    char c[3] = { 'a', 'x', 'p' };
-    cout << "정수배열의 최대값은" << getmax<int>(a,5) << endl;
+    int a[5] = { -5, 10, 30, 20, 6 };        //int형 배열 선언 후 값 초기화
+    double b[4] = { 3.14, 1.5, -6.0, 0.5 };  //double형 배열 선언 후 값 초기화
+    char c[3] = { 'a', 'x', 'p' };           //char형 배열 선언 후 값 초기화
+    
+    cout << "정수배열의 최대값은" << getmax<int>(a,5) << endl;        
     cout << "실수배열의 최대값은" << getmax<double>(b,4) << endl;
     cout << "문자배열의 최대값은" << getmax<char>(c,3) << endl;
         return 0;
