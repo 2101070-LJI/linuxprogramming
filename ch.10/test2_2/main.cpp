253e2755bbb90ed2b5783dd7bdd6fcedd5325df9
2101070-LJI <jungin5258@gmail.com>
2024-11-18 15:29:08 +0900

    Add files via upload

---
+++ ch.10/test2_2/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+template <typename T>
+class Container{
+    T *data;
+    int x;
+public:
+    Container(int i);
+    void set(int i, T a);
+    T get(int i);
+};
+
+template <typename T>
+Container<T>::Container(int i){ x=i; data=new T[i];}
+
+template <typename T>
+void Container<T>::set(int i, T a){
+    data[i]=a;
+}
+
+template <typename T>
+T Container<T>::get(int i){
+    return data[i];
+}
+
+int main() {
+Container<char> c(26);
+for (int i = 0; i < 26; i++)
+c.set(i, 'a' + i);
+for (int i = 25; i >= 0; i--)
+cout << c.get(i);
+cout << endl;
+}
